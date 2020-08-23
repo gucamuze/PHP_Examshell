7c19439fa5b510438c25a942394c4aae6571b24c
Nitha <guillaume.camuzeaux@epitech.eu>
2020-08-23 21:52:29 +0200

    Almost functional, heavy work done on main program loop. WIP -> next exercise, check results / score

--- examshell.class.php
+++ examshell.class.php
@@ -2,50 +2,104 @@
 
 class Examshell
 {
-    private $userInput;
-    private $score, $startTime, $currentTime, $timeLimit = 0;
+    private $userInput, $score, $startTime, $currentTime, $timeLimit, $pointsPerExercise;
+    private $currentExercise = 0;
     private $exercises, $help = [];
     private $on = true;
 
     function __construct()
     {
         $this->parse_examshell_json();
-        $this->startTime = time();
         $this->display_welcome_message();
+        // echo "pp exercises = " . $this->pointsPerExercise . PHP_EOL;
         $this->userInput = fgets(STDIN);
         if ($this->userInput == "\n") {
+            $this->startTime = time();
+            $this->start_exercise();
         }
-        if ($this->userInput == "time\n") {
-            $this->currentTime = time();
-            echo strftime("%H:%M:%S", 14400 - ($this->currentTime - $this->startTime));
+        // if ($this->userInput == "time\n") {
+        //     $this->currentTime = time();
+        //     echo strftime("%H:%M:%S", 14400 - ($this->currentTime - $this->startTime));
+        // }
+    }
+
+    private function start_exercise()
+    {
+        $this->create_repo();
+        $this->shell_display();
+    }
+
+    private function create_repo()
+    {
+        $currentExerciseArray = $this->exercises[$this->currentExercise];
+        $currentExerciseName = $currentExerciseArray["name"];
+        $currentExerciseInstructions = $currentExerciseArray["instructions"];
+        $currentExerciseShellInstructions = $currentExerciseArray["shellInstructions"];
+
+        if (!is_dir("./" . $currentExerciseArray["name"])) {
+            @system("mkdir " . $currentExerciseArray["name"]);
+            @system("touch " . $currentExerciseName . "/" . $currentExerciseName . ".txt");
+            file_put_contents("./" . $currentExerciseName . "/" . $currentExerciseName . ".txt", $currentExerciseInstructions);
+        }
+
+        echo $currentExerciseShellInstructions;
+    }
+
+    private function compile_and_check()
+    {
+        $currentExerciseArray = $this->exercises[$this->currentExercise];
+        $currentExerciseName = $currentExerciseArray["name"];
+        $currentExerciseExpectedOutput = $currentExerciseArray["expectedOutput"];
+        $result = null;
+
+        try {
+            if (file_exists("./" . $currentExerciseName . "/" . $currentExerciseName . ".c")) {
+                @system("gcc ./" . $currentExerciseName . "/" . $currentExerciseName . ".c");
+                @system("./a.out > result.yo");
+                @system("rm ./a.out");
+                $result = fread(fopen("./result.yo", "r"), filesize("./result.yo"));
+                @system("rm result.yo");
+            } else {
+                throw new Exception();
+            }
+        } catch (\Throwable $th) {
+            echo "\033[0;31mERROR : .c File not found in directory !\033[0m" . PHP_EOL;
+        }
+
+        if ($result == $currentExerciseExpectedOutput) {
+        } else {
+            echo "\033[0;31mERROR : expected output not matching results !\033[0m" . PHP_EOL;
+            echo "\033[0;31m>>>>> FAILURE :x Try again !\033[0m" . PHP_EOL . "Press enter when you're done :)" . PHP_EOL;
         }
     }
 
-    // function __destruct()
-    // {
-    //     $this->currentTime = time();
-    //     echo $this->currentTime - $this->startTime . PHP_EOL;
-    //     echo strftime("%H:%M:%S", $this->currentTime - $this->startTime) . PHP_EOL;
-    //     echo strftime("%H:%M:%S", 14400 - ($this->currentTime - $this->startTime));
-    // }
-
-    private function display_welcome_message()
+    private function shell_display()
     {
-        @system("clear");
-        $logo = fread(fopen("./logo.asciiart", "r"), filesize("./logo.asciiart"));
-        echo $logo . PHP_EOL;
-        echo "Welcome to PHP Examshell 00 ! Please read the README.md file before continuing" . PHP_EOL . "Press enter to begin..." . PHP_EOL;
+        while ($this->on == true) {
+            $this->userInput = fgets(STDIN);
+            if ($this->userInput == "\n") {
+                // $success = false;
+                $success = $this->compile_and_check();
+                echo "success value = " .  $success . PHP_EOL;
+                if ($success == true) {
+                    $this->exercice++;
+                    // $this->createRepo($this->listeExercices[$this->exercice]);
+                    break;
+                }
+            }
+        }
     }
 
     private function parse_examshell_json()
     {
         try {
-            if (file_exists("./exam.json")) {
-                $json = fread(fopen("./exam.json", "r"), filesize("./exam.json"));
+            if (file_exists("./assets/exam.json")) {
+                $json = fread(fopen("./assets/exam.json", "r"), filesize("./assets/exam.json"));
                 $arrayFromJson = json_decode($json, true);
                 $this->timeLimit = (int) $arrayFromJson["timeLimit"];
                 $this->exercises = $arrayFromJson["exercises"];
                 $this->help = $arrayFromJson["help"];
+                $this->pointsPerExercise = 100 / count($this->exercises);
             } else {
                 throw new Exception();
             }
@@ -53,26 +107,13 @@ class Examshell
             echo "Fatal error : mandatory exam.json file missing" . PHP_EOL . "Exiting...";
             die;
         }
-        // var_dump($arrayFromJson["exercises"]);
-        // foreach ($arrayFromJson as $key => $value) {
-        //     var_dump($key);
-        //     var_dump($value);
-        // }
     }
 
-    // private function shellDisplay()
-    // {
-    //     while ($this->on == true) {
-    //         $this->userInput = fgets(STDIN);
-    //         if ($this->userInput == "\n") {
-    //             $success = $this->testResult($this->listeExercices[$this->exercice]);
-    //             if ($success == true) {
-    //                 $this->exercice++;
-    //                 $this->createRepo($this->listeExercices[$this->exercice]);
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
-
+    private function display_welcome_message()
+    {
+        @system("clear");
+        $logo = fread(fopen("./assets/logo.asciiart", "r"), filesize("./assets/logo.asciiart"));
+        echo $logo . PHP_EOL;
+        echo "Welcome to PHP Examshell 00 ! Please read the README.md file before continuing" . PHP_EOL . "Press enter to begin..." . PHP_EOL;
+    }
 }

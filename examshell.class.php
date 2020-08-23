2a6ca59b7baf9bf4ac27e9dde779748035d584df
Nitha <guillaume.camuzeaux@epitech.eu>
2020-08-23 20:46:16 +0200

    Json parser method OK, WIP -> infinite loop and time limit implementation

--- examshell.class.php
+++ examshell.class.php
@@ -1,14 +1,15 @@
 <?php
 
-class Examshell 
+class Examshell
 {
-    private $userInput, $startTime, $currentTime;
+    private $userInput;
+    private $score, $startTime, $currentTime, $timeLimit = 0;
+    private $exercises, $help = [];
     private $on = true;
-    private $score = 0;
-    private $timeLimit = 14400; // 14.400 seconds -> 4 hours
 
     function __construct()
     {
+        $this->parse_examshell_json();
         $this->startTime = time();
         $this->display_welcome_message();
         $this->userInput = fgets(STDIN);
@@ -16,39 +17,62 @@ class Examshell
         }
         if ($this->userInput == "time\n") {
             $this->currentTime = time();
-            echo strftime("%H:%M:%S", 14400 - ($this-> currentTime - $this->startTime));
+            echo strftime("%H:%M:%S", 14400 - ($this->currentTime - $this->startTime));
         }
     }
 
-    function __destruct()
-    {
-        $this->currentTime = time();
-        echo $this-> currentTime - $this->startTime . PHP_EOL;
-        echo strftime("%H:%M:%S", $this-> currentTime - $this->startTime) . PHP_EOL;
-        echo strftime("%H:%M:%S", 14400 - ($this-> currentTime - $this->startTime));
-        
-    }
+    // function __destruct()
+    // {
+    //     $this->currentTime = time();
+    //     echo $this->currentTime - $this->startTime . PHP_EOL;
+    //     echo strftime("%H:%M:%S", $this->currentTime - $this->startTime) . PHP_EOL;
+    //     echo strftime("%H:%M:%S", 14400 - ($this->currentTime - $this->startTime));
+    // }
 
-    private function display_welcome_message() {
+    private function display_welcome_message()
+    {
         @system("clear");
         $logo = fread(fopen("./logo.asciiart", "r"), filesize("./logo.asciiart"));
         echo $logo . PHP_EOL;
-        echo "Welcome to PHP Examshell 00 ! Please read the readme file before continuing" . PHP_EOL . "Press enter to begin..." . PHP_EOL;
+        echo "Welcome to PHP Examshell 00 ! Please read the README.md file before continuing" . PHP_EOL . "Press enter to begin..." . PHP_EOL;
     }
 
-    private function shellDisplay()
+    private function parse_examshell_json()
     {
-        while ($this->on == true) {
-            $this->userInput = fgets(STDIN);
-            if ($this->userInput == "\n") {
-                $success = $this->testResult($this->listeExercices[$this->exercice]);
-                if ($success == true) {
-                    $this->exercice++;
-                    $this->createRepo($this->listeExercices[$this->exercice]);
-                    break;
-                }
+        try {
+            if (file_exists("./exam.json")) {
+                $json = fread(fopen("./exam.json", "r"), filesize("./exam.json"));
+                $arrayFromJson = json_decode($json, true);
+                $this->timeLimit = (int) $arrayFromJson["timeLimit"];
+                $this->exercises = $arrayFromJson["exercises"];
+                $this->help = $arrayFromJson["help"];
+            } else {
+                throw new Exception();
             }
+        } catch (\Throwable $th) {
+            echo "Fatal error : mandatory exam.json file missing" . PHP_EOL . "Exiting...";
+            die;
         }
+        // var_dump($arrayFromJson["exercises"]);
+        // foreach ($arrayFromJson as $key => $value) {
+        //     var_dump($key);
+        //     var_dump($value);
+        // }
     }
+
+    // private function shellDisplay()
+    // {
+    //     while ($this->on == true) {
+    //         $this->userInput = fgets(STDIN);
+    //         if ($this->userInput == "\n") {
+    //             $success = $this->testResult($this->listeExercices[$this->exercice]);
+    //             if ($success == true) {
+    //                 $this->exercice++;
+    //                 $this->createRepo($this->listeExercices[$this->exercice]);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
 
 }
